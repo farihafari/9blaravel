7c5db4cd1dedf2a665feb8606236ffc8c1c83594
farihafari <fareehajabeen62@gmail.com>
2024-11-06 09:17:48 +0500

    template done

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('panel/index');
+});
+Route::get('/home', function () {
+    return view('index');
+});
